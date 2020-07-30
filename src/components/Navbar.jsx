ccc6a2ad56e6475355029e0beab96b7a895411dc
Matteo Fenu <matfen97@gmail.com>
2020-07-30 08:31:06 +0200

    Added the Navbar and NavbarItem structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
 import Navitem from './Navitem';
 class Navbar extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = { 'NavItemActive': '' }
+    }
+    activeitem = (x) => {
+        if (this.state.NavItemActive.length > 0) {
+            document.getElementById(this.state.NavItemActive).classList.remove('active');
+        }
+        this.setState({ 'NavItemId': x }, () => {
+            document.getElementById(this.state.NavItemActive).classList.add('active');
+        });
+    };
     render() {
         return (
             <nav>
-                <ul>
-                    <Navitem item="Home" tolink="/" ></Navitem>
-                    <Navitem item="About" tolink="/about" ></Navitem>
-                    <Navitem item="Education" tolink="/education"></Navitem>
-                    <Navitem item="Skills" tolink="/skills"></Navitem>
-                    <Navitem item="Contact" tolink="/contact"></Navitem>
-                </ul>
-            </nav>
+                <ul><Navitem item="Home" tolink="/" activec={this.activeitem}>
+                </Navitem>
+                    <Navitem item="About" tolink="/about" activec={this.activeitem}>
+                    </Navitem>
+                    <Navitem item="Projects" tolink="/projects" activec={this.activeitem}>
+                    </Navitem>
+                    <Navitem item="Skills" tolink="/skills" activec={this.activeitem}>
+                    </Navitem>
+                    <Navitem item="Contact" tolink="/contact" activec={this.activeitem}>
+                    </Navitem></ul></nav>
         )
     }
 }
-
 export default Navbar
