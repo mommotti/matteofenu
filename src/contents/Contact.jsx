bb282677dd55f009193cc28d2d96b8e2b38d605a
Matteo Fenu <matfen97@gmail.com>
2020-07-31 11:37:28 +0200

    implementing form

--- src/contents/Contact.jsx
+++ src/contents/Contact.jsx
@@ -6,7 +6,7 @@ class Contact extends Component {
                 <h1 className="subtopic">Contact Me</h1>
                 <p>email: matfen97@gmail.com</p>
 
-                <form name="contact" method="POST" data-netlify="true">
+                <form name="contact" method="POST" data-netlify="true" autocomplete="off" data-netlify-recaptcha="true">
                     <input type="hidden" name="form-name" value="contact" />
                     <p>
                         <label>Your Name: <input type="text" name="name" /></label>
@@ -14,25 +14,17 @@ class Contact extends Component {
                     <p>
                         <label>Your Email: <input type="email" name="email" /></label>
                     </p>
-                    {/* <p>
-                        <label>Your Role: <select name="role[]" multiple>
-                            <option value="inquire">Inquire</option>
-                            <option value="other">Other</option>
-                        </select>
-                        </label>
-                    </p> */}
-
                     <label for="inquire">Inquire</label>
                     <input type="radio" id="inquire" name="role" value="inquire"></input>
                     <label for="other">Other</label>
                     <input type="radio" id="other" name="role" value="other"></input>
-
                     <p>
                         <label>Message: <textarea name="message"></textarea></label>
                     </p>
                     <p>
                         <div class="captcha" data-netlify-recaptcha="true"></div>
                     </p>
+                    <div data-netlify-recaptcha="true"></div>
                     <p>
                         <button type="submit">Send</button>
                     </p>
