2ff94d5c6c6f5ba06ae1e0a80c14d51dbd4a3f88
Matteo Fenu <matfen97@gmail.com>
2020-07-31 13:17:45 +0200

    implementing form

--- src/contents/Contact.jsx
+++ src/contents/Contact.jsx
@@ -4,30 +4,40 @@ class Contact extends Component {
         return (
             <div className="condiv">
                 <h1 className="subtopic">Contact Me</h1>
-                <p>email: matfen97@gmail.com</p>
-                <form name="contact" method="POST" data-netlify="true" autocomplete="off" data-netlify-recaptcha="true">
-                    <input type="hidden" name="form-name" value="contact" />
-                    <p>
-                        <label>Your Name: <input type="text" name="name" /></label>
-                    </p>
-                    <p>
-                        <label>Your Email: <input type="email" name="email" /></label>
-                    </p>
-                    <label for="inquire">Inquire</label>
-                    <input type="radio" id="inquire" name="role" value="inquire"></input>
-                    <label for="other">Other</label>
-                    <input type="radio" id="other" name="role" value="other"></input>
-                    <p>
-                        <label>Message: <textarea name="message"></textarea></label>
-                    </p>
-                    <p>
-                        <div class="captcha" data-netlify-recaptcha="true"></div>
-                    </p>
-                    <div data-netlify-recaptcha="true"></div>
-                    <p>
-                        <button type="submit">Send</button>
-                    </p>
-                </form>
+                <div className="mf-form">
+                    <form name="contact" method="POST" data-netlify="true" autocomplete="off" data-netlify-recaptcha="true">
+                        <input type="hidden" name="form-name" value="contact" />
+                        <p>
+                            <label>Your Name: <input className="input-info" type="text" name="name" /></label>
+                        </p>
+                        <hr className="form-hr" />
+                        <p>
+                            <label>Your Email: <input className="input-info" type="email" name="email" /></label>
+                        </p>
+                        <hr className="form-hr" />
+                        <div>
+                            <p className="align-radio">
+                                <label for="inquire">Inquire</label>
+                                <input type="radio" id="inquire" name="role" value="inquire"></input>
+                            </p>
+                            <p className="align-radio">
+                                <label for="other">Other</label>
+                                <input type="radio" id="other" name="role" value="other"></input>
+                            </p>
+                        </div>
+                        <hr className="form-hr" />
+                        <p className="align-radio">
+                            <label>Message: <textarea name="message"></textarea></label>
+                        </p>
+                        <p>
+                            <div class="captcha" data-netlify-recaptcha="true"></div>
+                        </p>
+                        <div data-netlify-recaptcha="true"></div>
+                        <p>
+                            <button className="btn" type="submit">Send</button>
+                        </p>
+                    </form>
+                </div>
             </div>)
     }
 }
