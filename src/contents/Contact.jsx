752e6feca03db43bd3b1e6b1c9a30b631c01a390
Matteo Fenu <matfen97@gmail.com>
2020-07-31 11:12:28 +0200

    implementing form

--- src/contents/Contact.jsx
+++ src/contents/Contact.jsx
@@ -7,6 +7,7 @@ class Contact extends Component {
                 <p>email: matfen97@gmail.com</p>
 
                 <form name="contact" method="POST" data-netlify="true">
+                    <input type="hidden" name="form-name" value="contact" />
                     <p>
                         <label>Your Name: <input type="text" name="name" /></label>
                     </p>
