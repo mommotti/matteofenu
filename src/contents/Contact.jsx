c832fc427276a9aa7d9f4a89c98467947af80bb4
Matteo Fenu <matfen97@gmail.com>
2020-07-31 11:53:19 +0200

    implementing form

--- src/contents/Contact.jsx
+++ src/contents/Contact.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import NetlifyForm from 'react-netlify-form'
 class Contact extends Component {
     render() {
         return (
             <div className="condiv">
                 <h1 className="subtopic">Contact Me</h1>
                 <p>email: matfen97@gmail.com</p>
-
+                {/* 
                 <form name="contact" method="POST" data-netlify="true" autocomplete="off" data-netlify-recaptcha="true">
                     <input type="hidden" name="form-name" value="contact" />
                     <p>
@@ -28,8 +29,30 @@ class Contact extends Component {
                     <p>
                         <button type="submit">Send</button>
                     </p>
-                </form>
+                </form> */}
 
+                <NetlifyForm name='contact'>
+                    {({ loading, error, success }) => (
+                        <div>
+                            {loading &&
+                                <div>Loading...</div>
+                            }
+                            {error &&
+                                <div>Your information was not sent. Please try again later.</div>
+                            }
+                            {success &&
+                                <div>Thank you for contacting us!</div>
+                            }
+                            {!loading && !success &&
+                                <div>
+                                    <input type='text' name='Name' required />
+                                    <textarea name='Message' required />
+                                    <button>Submit</button>
+                                </div>
+                            }
+                        </div>
+                    )}
+                </NetlifyForm>
             </div>)
     }
 }
