208b76c4a30f945ef594743bc21bc6d3d118c0ef
Matteo Fenu <matfen97@gmail.com>
2020-07-31 21:18:52 +0200

    editing the post

--- src/contents/Contact.jsx
+++ src/contents/Contact.jsx
@@ -5,7 +5,7 @@ class Contact extends Component {
             <div className="condiv">
                 <h1 className="subtopic">Contact Me</h1>
                 <div className="mf-form">
-                    <form action="/" name="contact" method="POST" data-netlify="true" autocomplete="off">
+                    <form name="contact" method="POST" data-netlify="true" autocomplete="off">
                         <input type="hidden" name="form-name" value="contact" />
                         <p className="input-p"><label>Your Name:</label></p>
                         <p>
@@ -33,7 +33,6 @@ class Contact extends Component {
                         </p>
                         <p>
                         </p>
-                        <div data-netlify-recaptcha="true"></div>
                         <p>
                             <button className="btn" type="submit">Send</button>
                         </p>
