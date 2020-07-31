34dc0bad0536903aa385fa3f96bd1eeb54ee6f44
Matteo Fenu <matfen97@gmail.com>
2020-07-31 10:57:20 +0200

    implementing form

--- src/contents/Contact.jsx
+++ src/contents/Contact.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import NetlifyForm from 'react-netlify-form'
 class Contact extends Component {
     render() {
         return (
@@ -7,41 +6,25 @@ class Contact extends Component {
                 <h1 className="subtopic">Contact Me</h1>
                 <p>email: matfen97@gmail.com</p>
 
-                <NetlifyForm
-                    name='Form With Invisible Recaptcha'
-                    recaptcha={{
-                        sitekey: 'my_recaptcha_site_key',
-                        size: 'invisible'
-                    }}
-                >
-                    {({ loading, error, recaptchaError, success, recaptcha }) => (
-                        <div>
-                            {loading &&
-                                <div>Loading...</div>
-                            }
-                            {error &&
-                                <div>Your information was not sent. Please try again later.</div>
-                            }
-                            {recaptchaError &&
-                                <div>Recaptcha did not match. Please make sure the box is checked.</div>
-                            }
-                            {success &&
-                                <div>Thank you for contacting us!</div>
-                            }
-                            {!loading && !success &&
-                                <div>
-                                    <input type='text' name='Name' required />
-                                    <textarea name='Message' required />
-                                    <button>Submit</button>
-                                </div>
-                            }
-                            {/* Invisible reCAPTCHA must be kept outside of conditionals */}
-                            {recaptcha}
-                        </div>
-                    )}
-                </NetlifyForm>
+                <form name="contact" method="post">
+                    <input type="hidden" name="form-name" value="contact" />
+                    <p>
+                        <label>Your Name: <input type="text" name="name" /></label>
+                    </p>
+                    <p>
+                        <label>Your Email: <input type="email" name="email" /></label>
+                    </p>
+                    <p>
+                        <label>Message: <textarea name="message"></textarea></label>
+                    </p>
+                    <p>
+                        <button type="submit">Send</button>
+                    </p>
+                </form>
 
-            </div>)
+            </div>
+
+        )
     }
 }
 export default Contact
