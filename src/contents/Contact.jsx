e74bcd74a6cb82ff19b8d9d3e7130030e4c9c601
Matteo Fenu <matfen97@gmail.com>
2020-07-31 10:23:04 +0200

    implementing form

--- src/contents/Contact.jsx
+++ src/contents/Contact.jsx
@@ -6,13 +6,18 @@ class Contact extends Component {
                 <h1 className="subtopic">Contact Me</h1>
                 <p>email: matfen97@gmail.com</p>
 
-                <form name="contact" netlify>
-                    <input type="hidden" name="form-name" value="contact" />
+                <form name="contact" method="POST" data-netlify="true">
                     <p>
                         <label>Your Name: <input type="text" name="name" /></label>
                     </p>
                     <p>
                         <label>Your Email: <input type="email" name="email" /></label>
+                    </p>
+                    <p>
+                        <label>Your Role: <select name="role[]" multiple>
+                            <option value="leader">Leader</option>
+                            <option value="follower">Follower</option>
+                        </select></label>
                     </p>
                     <p>
                         <label>Message: <textarea name="message"></textarea></label>
@@ -21,6 +26,7 @@ class Contact extends Component {
                         <button type="submit">Send</button>
                     </p>
                 </form>
+
             </div>)
     }
 }
