a725ed04ee7d4e6b0f6001c13dfbeaecd726238f
Matteo Fenu <matfen97@gmail.com>
2020-08-02 17:55:24 +0200

    created many modules

--- src/contents/Projects.jsx
+++ src/contents/Projects.jsx
@@ -36,6 +36,39 @@ class Projects extends Component {
                             </div>
                         </div>
                     </div>
+
+
+                    <div class="project-card">
+                        <div class="project-preview">
+                            <div class="project-description">
+
+                                <h1>Vue Todo</h1>
+                                <div class="separator"></div>
+                                <h3 class="h3-space">Technologies used:</h3>
+                                <div class="technologies">
+                                    <p>VueJS</p>
+                                    <p>ExpressJS</p>
+                                    <p>MongoDB</p>
+                                </div>
+                                <div class="separator"></div>
+                                <h3 class="h3-space">Goal of the project:</h3>
+                                <div class="project-goal">
+                                    <p>
+                                        The goal of this project is to get a better understanding of how the back-end and the
+                            front-end are connected and work together. <br></br> I built a basic To-Do app by using MongoDB
+                                        as the
+                                        database,
+                            ExpressJS as the server, and VueJS as the front-end framework. <br></br>
+                                                I implemented basic CRUD functionality.
+                        </p>
+                                </div>
+                                <div class="separator"></div>
+                                <h3 class="h3-space">Check it out:</h3>
+                                <a href="https://todo-vue-mf.herokuapp.com/" target="_blank"><button class="project-button">Click
+                            Here</button></a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <p style={{ color: 'red' }}>| More projects to come... |</p>
             </div>)
