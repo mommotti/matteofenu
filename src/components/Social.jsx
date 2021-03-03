144516831663b9eb1feda16a423ae0e354631f1a
Matteo Fenu <matfen97@gmail.com>
2021-03-03 14:49:50 +0100

    edited

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -5,7 +5,7 @@ class Social extends Component {
             <div className="copy">
                 <div class="social">
                     <a href="https://github.com/mommotti" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
-                    <a href="https://www.instagram.com/mommottix/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
+                    {/* <a href="https://www.instagram.com/mommottix/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a> */}
                     <a href="https://www.facebook.com/matteofenu97/" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
                     <a href="https://www.linkedin.com/in/fenumatteo/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
                     <a href="https://twitter.com/mommottee" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter"></i></a>
