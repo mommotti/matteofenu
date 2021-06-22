7d8b4f0f28361aeea10b58a091462242fe6ac152
Matteo Fenu <matteofenu@Matteos-Air.home>
2021-06-22 23:14:45 +0200

    edited

--- src/contents/UX.jsx
+++ src/contents/UX.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 class About extends Component {
     render() {
@@ -12,6 +13,8 @@ class About extends Component {
                     <p>After being part of the biggest Italian UI contest, <a  style={{color: "black", fontWeight: 700, textDecoration: "underline"}} href="https://designflows.it/" target="_blank">Designflows 2020</a> with <a  style={{color: "black", fontWeight: 700, textDecoration: "underline"}} href="https://matteofenu.netlify.app/assets/projectImgs/ai8.jpg" target="_blank">this</a> entry, 
                     I realized where my passion lies in: <p className="design-text-design">DESIGN !</p>
                     <br></br>
+                        <img className="image-ux"src={require('../img/images/ai8.jpg')} />
+                    <br></br>
                     <br></br>
                     <hr />
                     <br></br>
@@ -19,18 +22,24 @@ class About extends Component {
                     <h1 className="design-text">MY CORE CONCEPTS:</h1>
                     <br></br>
                     <br></br>
+                    <ScrollAnimation animateIn='bounceInRight' animateOnce="True">
                     <h2 className="design-text">01 - USER RESEARCH.</h2>
                     The first stage of UX design is all about empathizing with the target users and discovery more about them.
                     <br></br>During the course provided by Accenture, I learnt how to undertake effective user research methods. There are many ways to learn about the target users, interviews or focus groups with typical users are a great way to start gathering precious information.
                     <br></br>A good way to empathize with users is by giving them surveys and questionnaires about what they do right now and how they feel about it.
                     <br></br>Market research is another important aspect, using reports and statistics or anything else that is available.
                     <br></br>I'm currently learning more about user research, since it's the foundation of the UX design process.
-                    <br></br><br></br><h2 className="design-text">02 - PSYCHOLOGY.</h2>
+                    </ScrollAnimation>
+                    <br></br><br></br>
+                    <ScrollAnimation animateIn='bounceInRight' animateOnce="True"> <h2 className="design-text">02 - PSYCHOLOGY.</h2>
                     I have always been intrigued by the psychology behind people's problems and behaviours.
                     <br></br>This particular interest, plus my love for design, drove me to pursue a career in UX.
                     <br></br>I'm finding myself more and more passionate in learning how feelings, thoughts and actions can be improved by an amazing design.
                     <br></br>I'm a person that brainstorms a lot by nature, and knowing that a profession that embraces both beautiful designs and deep understanding of users psychology really sells this profession for me.
-                    <br></br><br></br><h2 className="design-text">03 - ACTUAL DESIGN.</h2>
+                    </ScrollAnimation>
+                    <br></br><br></br>
+                    <ScrollAnimation animateIn='bounceInRight' animateOnce="True">
+                    <h2 className="design-text">03 - ACTUAL DESIGN.</h2>
                     I employ a low to high fidelity approach.
                     <br></br>From information architecture to wireframes, to actual interactive prototypes.
                     <br></br>I start with pencil and paper, sticky notes, allowing myself to quickly record my thoughts and ideas on paper without any delay.
@@ -38,13 +47,16 @@ class About extends Component {
                     <br></br>For this reason, there is a need to quickly iterate over the wireframes that are not so intuitive, making them intuitive and clean.
                     <br></br>My approach to quickly re-create a part of the wireframe is to take an A4 paper and divide it into 8 equal parts. I then set an 8 minute timer, forcing myself to quickly draw each wireframe in just 1 minute each.
                     Once I'm happy with the wireframes, I translate the design into prototypes with apps such as Adobe XD, Figma or Sketch.
+                    </ScrollAnimation>
                     </p>
+
                     <br></br>
                     <br></br>
                     <br></br>
                     <br></br>
 
-                    <svg  viewBox="0 0 210 429" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                    <ScrollAnimation animateIn="fadeIn">
+                    <svg width="167px" height="429px" viewBox="0 0 167 429" version="1.1" xmlns="http://www.w3.org/2000/svg">
     <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
         <g id="-" transform="translate(-116.000000, -42.000000)">
             <g id="Group-10" transform="translate(116.000000, 42.000000)">
@@ -86,48 +98,48 @@ class About extends Component {
                         </text>
                     </g>
                 </g>
-                <g id="Group-6" transform="translate(108.000000, 0.000000)">
+                <g id="Group-6" transform="translate(137.000000, 0.000000)">
                     <g id="Group-7">
-                        <polygon id="Rectangle" fill-opacity="0.75" fill="#93A7FD" points="33.15 0 51 27.8987342 68.85 0 68.85 124.101266 51 152 33.15 124.101266"></polygon>
-                        <text id="U-N-D-E-R-S-T-A-N-D" font-family="OpenSans-Bold, Open Sans" font-size="7" font-weight="bold" line-spacing="9" fill="#FFFFFF">
-                            <tspan x="48.3544922" y="43">U</tspan>
-                            <tspan x="48.154541" y="52">N</tspan>
-                            <tspan x="48.4091797" y="61">D</tspan>
-                            <tspan x="49.0397949" y="70">E</tspan>
-                            <tspan x="48.6894531" y="79">R</tspan>
-                            <tspan x="49.0722656" y="88">S</tspan>
-                            <tspan x="48.9731445" y="97">T</tspan>
-                            <tspan x="48.5852051" y="106">A</tspan>
-                            <tspan x="48.154541" y="115">N</tspan>
-                            <tspan x="48.4091797" y="124">D</tspan>
+                        <polygon id="Rectangle" fill-opacity="0.75" fill="#93A7FD" points="0 0 15 27.8987342 30 0 30 124.101266 15 152 0 124.101266"></polygon>
+                        <text id="UNDERSTAND" font-family="OpenSans-Bold, Open Sans" font-size="7" font-weight="bold" fill="#FFFFFF">
+                            <tspan x="12.605042" y="43">U</tspan>
+                            <tspan x="12.605042" y="52">N</tspan>
+                            <tspan x="12.605042" y="61">D</tspan>
+                            <tspan x="13.0958173" y="70">E</tspan>
+                            <tspan x="12.7454755" y="79">R</tspan>
+                            <tspan x="13.128288" y="88">S</tspan>
+                            <tspan x="13.0291669" y="97">T</tspan>
+                            <tspan x="12.6412275" y="106">A</tspan>
+                            <tspan x="12.605042" y="115">N</tspan>
+                            <tspan x="12.605042" y="124">D</tspan>
                         </text>
                     </g>
                     <g id="Group-8" transform="translate(0.000000, 138.000000)">
-                        <polygon id="Rectangle-Copy" fill="#988CEA" points="33.15 0 51 27.8987342 68.85 0 68.85 124.101266 51 152 33.15 124.101266"></polygon>
-                        <text id="E-X-P-L-O-R-E" font-family="OpenSans-Bold, Open Sans" font-size="7" font-weight="bold" line-spacing="9" fill="#FFFFFF">
-                            <tspan x="49.0397949" y="53">E</tspan>
-                            <tspan x="48.6655273" y="62">X</tspan>
-                            <tspan x="48.8022461" y="71">P</tspan>
-                            <tspan x="49.0227051" y="80">L</tspan>
-                            <tspan x="48.2143555" y="89">O</tspan>
-                            <tspan x="48.6894531" y="98">R</tspan>
-                            <tspan x="49.0397949" y="107">E</tspan>
+                        <polygon id="Rectangle-Copy" fill="#988CEA" points="0 0 15 27.8987342 30 0 30 124.101266 15 152 0 124.101266"></polygon>
+                        <text id="EXPLORE" font-family="OpenSans-Bold, Open Sans" font-size="7" font-weight="bold" fill="#FFFFFF">
+                            <tspan x="13.0958173" y="52">E</tspan>
+                            <tspan x="12.7215498" y="61">X</tspan>
+                            <tspan x="12.8582685" y="70">P</tspan>
+                            <tspan x="13.0787275" y="79">L</tspan>
+                            <tspan x="12.605042" y="88">O</tspan>
+                            <tspan x="12.7454755" y="97">R</tspan>
+                            <tspan x="13.0958173" y="106">E</tspan>
                         </text>
                     </g>
-                    <g id="Group-9" transform="translate(34.000000, 277.000000)">
-                        <polygon id="Rectangle-Copy-2" fill="#A57CD3" points="0 0 17.5 27.8987342 35 0 35 124.101266 17.5 152 0 124.101266"></polygon>
+                    <g id="Group-9" transform="translate(1.000000, 277.000000)">
+                        <polygon id="Rectangle-Copy-2" fill="#A57CD3" points="0 0 14.5 27.8987342 29 0 29 124.101266 14.5 152 0 124.101266"></polygon>
                         <text id="M-A-T-E-R-I-A-L-I-Z" font-family="OpenSans-Bold, Open Sans" font-size="7" font-weight="bold" fill="#FFFFFF">
-                            <tspan x="14.5833333" y="42">M</tspan>
-                            <tspan x="15.0852051" y="51">A</tspan>
-                            <tspan x="15.4731445" y="60">T</tspan>
-                            <tspan x="15.5397949" y="69">E</tspan>
-                            <tspan x="15.1894531" y="78">R</tspan>
-                            <tspan x="16.3413086" y="87">I</tspan>
-                            <tspan x="15.0852051" y="96">A</tspan>
-                            <tspan x="15.5227051" y="105">L</tspan>
-                            <tspan x="16.3413086" y="114">I</tspan>
-                            <tspan x="15.4731445" y="123">Z</tspan>
-                            <tspan x="15.5397949" y="132">E</tspan>
+                            <tspan x="11.6" y="42">M</tspan>
+                            <tspan x="11.6018717" y="51">A</tspan>
+                            <tspan x="11.9898112" y="60">T</tspan>
+                            <tspan x="12.0564616" y="69">E</tspan>
+                            <tspan x="11.7061198" y="78">R</tspan>
+                            <tspan x="12.8579753" y="87">I</tspan>
+                            <tspan x="11.6018717" y="96">A</tspan>
+                            <tspan x="12.0393717" y="105">L</tspan>
+                            <tspan x="12.8579753" y="114">I</tspan>
+                            <tspan x="11.9898112" y="123">Z</tspan>
+                            <tspan x="12.0564616" y="132">E</tspan>
                         </text>
                     </g>
                 </g>
@@ -135,6 +147,8 @@ class About extends Component {
         </g>
     </g>
 </svg>
+                    </ScrollAnimation>               
+
 <br></br>
                     <br></br>
                     <br></br>
